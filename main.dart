0890cfddc2606c1fdc30eca4c6d19af9876cc639
mansidas19 <62935129+mansidas19@users.noreply.github.com>
2020-09-29 21:42:25 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:myanimation_app/myanim.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text('ANIMATION APP',),
+        ),
+        body: MyA1(),
+      ),
+    )
+  );
+}
+
